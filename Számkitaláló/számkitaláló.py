68b1289fc0ad3d9c157fcc538648b648dedf8d16
TheLightDelta <96254663+TheLightDelta@users.noreply.github.com>
2023-07-08 10:50:07 +0200

    Update számkitaláló.py
    Hátramaradt prób. szám rosszul volt kiírva

--- Számkitaláló/számkitaláló.py
+++ Számkitaláló/számkitaláló.py
@@ -4,10 +4,10 @@ print("Üdvözöllek! Gondoltam egy számra 1 és 100 között! Az a feladatod h
 
 random_num = random.randint(1, 100)
 
-prób_szám = 0
+prób_szám = 10
 
 while True:
-    prób_szám+=1
+    prób_szám-=1
     tipp = int(input("Kérlek add meg a tipped: "))
     
     if tipp == random_num:
@@ -15,15 +15,11 @@ while True:
         break
     elif tipp<random_num:
         print("Nagyobb számra gondoltam! Hátramaradt próbálkozások száma: ",prób_szám)
-        if prób_szám==10:
+        if prób_szám==0:
             print("Sajnos kifogytál a próbálkozásokból!")
             break
     else:
         print("Kissebb számra gondoltam! Hátramaradt próbálkozások száma: ",prób_szám)
-        if prób_szám==10:
+        if prób_szám==0:
             print("Sajnos kifogytál a próbálkozásokból! Legközelebb ügyesebb leszel!")
             break
-
-    
-
-
